367d5d8db5643555f339d7faf1ca9c6dcf98bbb2
Daniyar Salikhov <salihovdr@gmail.com>
2018-07-12 12:41:43 -0400

    Add Folder model, folders.json; update Note Model, notes.json, seed-databse.js

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -7,16 +7,29 @@ const mongoose = require('mongoose');
 const { MONGODB_URI } = require('../config');
 //import Note Schema and Model from note.js
 const Note = require('../models/note');
+//import Folder model
+const Folder = require('../models/folder');
 //import array of objects from db/see/notes.json
 const seedNotes = require('../db/seed/notes');
+//import array of folder objects
+const seedFolders = require('../db/seed/folders');
 
 mongoose.connect(MONGODB_URI)
   .then(() => mongoose.connection.db.dropDatabase())
-  .then(() => Note.insertMany(seedNotes))
+  .then(() => {
+    return Promise.all([
+      Note.insertMany(seedNotes),
+      Folder.insertMany(seedFolders),
+      Folder.createIndexes()
+    ]);
+  })
   .then(results => {
-    console.info(`Inserted ${results.length} Notes`);
+    console.info(`completed all ${results.length} tasks`);
   })
   .then(() => mongoose.disconnect())
   .catch(err => {
     console.error(err);
   });
+
+//seeding mlab db
+//export MONGODB_URI = mongodb://dcs:pw@ds233551.mlab.com:33551/dans-notes-db
